70c92babb1d3cfbc353c75ee2c2c9e45e2a62dee
chochua <jossue.avll@gmail.com>
2023-04-22 02:11:03 -0700

    Se agregaron mas weas

---
+++ src/Elementos/FuncionesMetodoGrafico.java
@@ -0,0 +1,10 @@
+package Elementos;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class FuncionesMetodoGrafico extends JFrame {
+    public static void valoresTiendenA_Cero(){
+        
+    }
+}
--- src/Elementos/Interfaz.java
+++ src/Elementos/Interfaz.java
@@ -1,11 +1,8 @@
 
 package Elementos;
 
-import org.w3c.dom.events.EventTarget;
-
 import javax.swing.*;
 import java.awt.*;
-import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -22,23 +19,30 @@ public class Interfaz extends JFrame {
     private TextField txtFox1,txtFox2,txtR1x1,txtR1x2,txtR2x1,txtR2x2,txtR3x1,
             txtR3x2,txtR1Coeficiente,txtR2Coeficiente,txtR3Coeficiente;
     private JLabel l_Izquierda, l_Centro, l_Derecho,l_Objetivo,l_Fox1,l_Fox2,
-            l_Restriccion1,l_Restriccion2,l_Restriccion3,l_Rr1x1,l_Rr1x2,l_Rr2x1,l_Rr2x2,l_Rr3x1,l_Rr3x2;
+            l_Restriccion1,l_Restriccion2,l_Restriccion3,l_Rr1x1,l_Rr1x2,l_Rr2x1,l_Rr2x2,l_Rr3x1,l_Rr3x2,l_FuncionObjetivo
+            ,l_SujetoA,l_SujetoARestriccion1,l_SujetoARestriccion2,l_SujetoARestriccion3, l_Restriccion1Fija, l_Restriccion2Fija, l_Restriccion3Fija,
+            l_FuncionObjetivoFija;
 
-    private JButton b_Limpiar,b_Calcular,b_AgregarR,b_BorrarR;
+    private JButton b_Limpiar, b_Fijar,b_AgregarR,b_BorrarR,b_Graficar;
     private JComboBox j_Objetivo, j_Desigualdad1,j_Desigualdad2,j_Desigualdad3;
 
     public Interfaz(){
-              setLayout(null);
+        setLayout(null);
         //Creacion de los componentes
         Image icon = Toolkit.getDefaultToolkit().getImage("C:\\Users\\jossu\\OneDrive\\Escritorio\\MetodoGrafico\\MetodoGrafico\\src\\gato.jpg");
         l_Objetivo = new JLabel("Objetivo:");
         b_Limpiar = new JButton("Limpiar");
-        b_Calcular = new JButton("Calcular");
+        b_Fijar = new JButton("Fijar");
         b_AgregarR = new JButton("Agregar Restriccion");
         b_BorrarR = new JButton("Borrar Restriccion");
+        b_Graficar  = new JButton("Graficar");
         l_Restriccion1 = new JLabel("Restriccion 1:");
         l_Restriccion2 = new JLabel("Restriccion 2:");
         l_Restriccion3 = new JLabel("Restriccion 3:");
+        l_SujetoA = new JLabel("Sujeto A:");
+        l_SujetoARestriccion1 = new JLabel("Restriccion 1:");
+        l_SujetoARestriccion2 = new JLabel("Restriccion 2:");
+        l_SujetoARestriccion3 = new JLabel("Restriccion 3:");
         l_Fox1 = new JLabel("X1 +");
         l_Fox2 = new JLabel("X2");
         l_Rr1x1 = new JLabel("X1 +");
@@ -47,8 +51,13 @@ public class Interfaz extends JFrame {
         l_Rr2x2 = new JLabel("X2");
         l_Rr3x1 = new JLabel("X1 +");
         l_Rr3x2 = new JLabel("X2");
+        l_FuncionObjetivoFija = new JLabel();
+        l_Restriccion1Fija = new JLabel();
+        l_Restriccion2Fija = new JLabel();
+        l_Restriccion3Fija = new JLabel();
         l_Derecho = new JLabel();
         l_Centro = new JLabel();
+        l_FuncionObjetivo = new JLabel();
         l_Izquierda = new JLabel();
         j_Objetivo = new JComboBox();
         j_Desigualdad1 = new JComboBox();
@@ -68,7 +77,7 @@ public class Interfaz extends JFrame {
 
         //Propiedades
         j_Objetivo.addItem("Maximizar");
-        j_Objetivo.addItem("Minizar");
+        j_Objetivo.addItem("Minimizar");
         j_Desigualdad1.addItem("≥");
         j_Desigualdad1.addItem("≤");
         j_Desigualdad1.addItem("=");
@@ -78,10 +87,11 @@ public class Interfaz extends JFrame {
         j_Desigualdad3.addItem("≥");
         j_Desigualdad3.addItem("≤");
         j_Desigualdad3.addItem("=");
-
+        //Funcion objetivo
         l_Fox1.setBounds(80,70,50,60);
         txtFox1.setBounds(15,90,60,25);
         txtFox2.setBounds(115,90,60,25);
+        l_FuncionObjetivoFija.setBounds(20,20,140,60);
 
         txtR1x1.setBounds(15,160,60,25);
         txtR1x2.setBounds(115,160,60,25);
@@ -90,19 +100,31 @@ public class Interfaz extends JFrame {
         txtR3x1.setBounds(15,300,60,25);
         txtR3x2.setBounds(115,300,60,25);
 
+        l_FuncionObjetivo.setBounds(20,20,80,60);
         l_Fox2.setBounds(185,70,50,60);
+        //Restriccion base 1
         l_Restriccion1.setBounds(20,120,80,60);
+        l_SujetoARestriccion1.setBounds(20,100,80,60);
+        l_Restriccion1Fija.setBounds(20,120,200,60);
         l_Rr1x1.setBounds(80,140,50,60);
         l_Rr1x2.setBounds(185,140,50,60);
         txtR1Coeficiente.setBounds(270,160,70,25);
+        //Restriccion base 2
         l_Restriccion2.setBounds(20,190,80,60);
+        l_SujetoARestriccion2.setBounds(20,170,80,60);
+        l_Restriccion2Fija.setBounds(20,190,200,60);
+
         l_Rr2x1.setBounds(80,210,50,60);
         l_Rr2x2.setBounds(185,210,50,60);
         txtR2Coeficiente.setBounds(270,230,70,25);
+        //Restriccion base 3
         l_Restriccion3.setBounds(20,260,80,60);
+        l_SujetoARestriccion3.setBounds(20,240,80,60);
         l_Rr3x1.setBounds(80,280,50,60);
         l_Rr3x2.setBounds(185,280,50,60);
         txtR3Coeficiente.setBounds(270,300,70,25);
+        l_Restriccion3Fija.setBounds(20,260,200,60);
+
         l_Objetivo.setBounds(20,10,100,50);
         j_Objetivo.setBounds(20,50,100,20);
         j_Desigualdad1.setBounds(220,160,40,25);
@@ -111,16 +133,28 @@ public class Interfaz extends JFrame {
         l_Izquierda.setBorder(BorderFactory.createLineBorder(Color.black));
         l_Izquierda.setBounds(0,0,400,600);
         l_Centro.setBorder(BorderFactory.createLineBorder(Color.black));
-        l_Centro.setBounds(410,0,400,600);
+        l_Centro.setBounds(410,0,400,700);
         l_Derecho.setBorder(BorderFactory.createLineBorder(Color.black));
         l_Derecho.setBounds(820,0,400,600);
+
         b_AgregarR.setBounds(20,450,150,40);
         b_BorrarR.setBounds(20,500,150,40);
-        b_Calcular.setBounds(200,450,150,40);
+        b_Fijar.setBounds(200,450,150,40);
         b_Limpiar.setBounds(200,500,150,40);
+        b_Graficar.setBounds(120,500,150,30);
 
+        //Comportamiento de los botones y los ComboBox
+        j_Objetivo.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if(j_Objetivo.getSelectedItem().equals("Maximizar")){
+                    l_FuncionObjetivo.setText("Maximizar");
+                }else{
+                    l_FuncionObjetivo.setText("Minimizar");
+                }
+            }
+        });
         b_Limpiar.addActionListener(new Eventos(txtFox1,txtFox2,txtR1x1,txtR1x2,txtR2x1,txtR2x2,txtR3x1,txtR3x2,txtR1Coeficiente,txtR2Coeficiente,txtR3Coeficiente));
-        b_Calcular.addActionListener(this::actionPerformed);
+        b_Fijar.addActionListener(this::actionPerformed);
         b_AgregarR.addActionListener(this::actionPerformed);
         b_BorrarR.addActionListener(this::actionPerformed);
     //Apartado de las 3 restricciones que van a estar ahi por mientras
@@ -153,10 +187,21 @@ public class Interfaz extends JFrame {
         l_Izquierda.add(l_Objetivo);
         l_Izquierda.add(b_AgregarR);
         l_Izquierda.add(b_BorrarR);
-        l_Izquierda.add(b_Calcular);
+        l_Izquierda.add(b_Fijar);
         l_Izquierda.add(b_Limpiar);
         l_Izquierda.add(j_Objetivo);
-        
+        //Contenido del JLabel del centro 
+        l_Centro.add(b_Graficar);
+        l_Centro.add(l_FuncionObjetivo);
+        l_Centro.add(l_SujetoARestriccion1);
+        l_Centro.add(l_SujetoARestriccion2);
+        l_Centro.add(l_SujetoARestriccion3);
+        l_Centro.add(l_Restriccion1Fija);
+        l_Centro.add(l_Restriccion2Fija);
+        l_Centro.add(l_Restriccion3Fija);
+        l_Centro.add(l_FuncionObjetivoFija);
+
+
         add(l_Derecho);
         add(l_Centro);
         setResizable(false);
@@ -179,9 +224,12 @@ public class Interfaz extends JFrame {
             //Borrar restriccion
             JOptionPane.showMessageDialog(null, "Borrar Restriccion");
         }
-        if(e.getSource()==b_Calcular){
+        if(e.getSource()== b_Fijar){
             //Calcular
-            JOptionPane.showMessageDialog(null, "Calcular");
+            l_Restriccion1Fija.setText(txtR1x1.getText()+"X1 + "+txtR1x2.getText()+"X2 "+j_Desigualdad1.getSelectedItem()+" "+txtR1Coeficiente.getText());
+            l_Restriccion2Fija.setText(txtR2x1.getText()+"X1 + "+txtR2x2.getText()+"X2 "+j_Desigualdad2.getSelectedItem()+" "+txtR2Coeficiente.getText());
+            l_Restriccion3Fija.setText(txtR3x1.getText()+"X1 + "+txtR3x2.getText()+"X2 "+j_Desigualdad3.getSelectedItem()+" "+txtR3Coeficiente.getText());
+            l_FuncionObjetivoFija.setText("Z=" + l_FuncionObjetivo.getText()+" "+txtFox1.getText()+"X1 + "+txtFox2.getText()+"X2");
         }
     }
 }
